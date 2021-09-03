76ab4bef0e0d02ebbc9f93917ba736d40532671c
David Keegan <git@davidkeegan.com>
2021-09-03 07:52:02 -0700

    added Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SSSwiftUIGIFView",
+    platforms: [
+        .iOS(.v13), .tvOS(.v13)
+    ],
+    products: [
+        .library(name: "SSSwiftUIGIFView", type: .dynamic, targets: ["SSSwiftUIGIFView"]),
+    ],
+    targets: [
+        .target(name: "SSSwiftUIGIFView", dependencies: []),
+    ]
+)
